3d7f0606be67c3b6b4bf32ae6e115342863bd71a
dl-ng <dilam.nguyen@gmail.com>
2016-03-14 00:13:38 +0100

    add account

---
+++ website/server/lib/accounts/onCreateUser.js
@@ -0,0 +1,20 @@
+Accounts.onCreateUser((options, user) => {
+    let     regex;
+
+    regex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+
+    if (regex.test(options.mail)) {
+        user.email = options.mail;
+    }
+    if (options.password === options.passwordConfirm) {
+        user.password = options.password;
+    }
+    if (options.username) {
+        user.username = options.username;
+    }
+    if (!options.cgu) {
+        return (false);
+    }
+
+    return user;
+});
